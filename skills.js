6f7b744b67d11434a4d9b529fdecbcf5bd0c13d1
andrewpinedaa <167822358+andrewpinedaa@users.noreply.github.com>
2025-05-07 08:41:33 -1000

    Create skills.js

---
+++ skills.js
@@ -0,0 +1,12 @@
+  <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const currentPage = window.location.pathname.split('/').pop();
+            const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+            
+            navLinks.forEach(link => {
+                if (link.getAttribute('href') === currentPage) {
+                    link.classList.add('active');
+                }
+            });
+        });
+    </script>
